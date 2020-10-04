611e076dcd856ba1c9da1e1c0798e9b441822c17
vagrant <vagrant@vagrant.vm>
2020-10-04 21:12:41 +0000

    Final commit

--- functions.py
+++ functions.py
@@ -21,7 +21,18 @@ Return:
     - True or False (bool)
 """
 
-# Write your function here
+def hometowns (my_hometown, your_hometown):
+    if my_hometown == your_hometown:
+        result = True
+    else:
+        result = False
+    return result
+    print(result)
+    print(type(result))
+
+my_hometown = 'Milwaukee'
+your_hometown = input("What is your hometown? ")
+hometowns(my_hometown, your_hometown)
 
 
 """PROMPT 2
@@ -39,7 +50,14 @@ Return:
     - Full name (str)
 """
 
-# Write your function here
+def add_names (first_name, last_name):
+    print(full_name)
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+full_name = first_name + ' ' + last_name
+
+add_names(first_name, last_name)
 
 
 """PROMPT 3
@@ -64,12 +82,19 @@ Arguments:
     - Hometown (str)
 """
 
-# Write your function here
+def greeting (full_name, your_hometown):
+    if my_hometown == your_hometown:
+        print(f"Hi {full_name}, we're from the same place!")
+    else:
+        print(f"Hi {full_name}, I'd like to visit {your_hometown}!")
+greeting (full_name, your_hometown)
 
 
 """PROMPT 4
 
 Write a function that returns True if a fruit is a berry.
+if this thing is on the berry list, then it is a berry
+
 
 Valid berries are:
     - strawberry
@@ -88,7 +113,15 @@ Return:
     - True or False (bool)
 """
 
-# Write your function here
+def is_berry(fruit):
+    if fruit in berries:
+        result = True
+    else:
+        result = False
+    print(result)
+
+berries = ['strawberry', 'raspberry', 'blackberry', 'currant']
+is_berry("strawberry")
 
 
 """PROMPT 5
@@ -103,8 +136,15 @@ Arguments:
 Return:
     - Shipping cost (int)
 """
-
-# Write your function here
+def shipping_cost(fruit):
+    berry_cost = 0
+    other_cost = 5
+    if fruit in berries:
+        shipped_cost = berry_cost
+    else:
+        shipped_cost = other_cost
+    print(f"The cost to ship a {fruit} is {shipped_cost}")
+shipping_cost("strawberry")
 
 
 """PROMPT 6
@@ -132,9 +172,26 @@ Arguments:
 Return:
     - Total price after taxes and fees (float)
 """
+def cost_of_item(cost, state, tax):
+    if state == "CA":
+        total_price = (cost * tax) + cost
+    else:
+        total_price = cost + tax
+    print(total_price)
 
-# Write your function here
+state = input("What state are you in? ")
+purchase = int(input("How much does it cost? "))
 
+if state == "CA":
+    tax = 0.03
+elif state == "PA":
+    tax = 2
+elif state == "MA":
+    if purchase <= 100:
+        tax = 1.0
+    else:
+        tax = 3.0
+cost_of_item(10, "CA", tax)
 
 """PROMPT 7
 
@@ -153,6 +210,10 @@ Arguments:
 Return:
     - A list with arguments added to the end (list)
 """
+
+#this function would require some sort of %arg or &arg function that I can't quite get yet
+#it also requires a potential "infinite" list
+#the end of the function would turn out the list that you were visualizing
 
 # Write your function here
 
@@ -182,3 +243,8 @@ Return:
 """
 
 # Write your function here
+
+#this function would start with a simple word - something like hi
+#you would then create a function that makes space for the "hi" to grow
+#you would multiply this list *3
+#the output would be -> 'hi', 'hihihi' to create the tuple
--- lists.py
+++ lists.py
@@ -5,95 +5,41 @@ Complete the following functions.
 
 
 def print_indices(items):
-    """Print each item in the list, followed by its index.
+    """Print each item in the list, followed by its index."""
+    for indices, item in enumerate(items):
+        print(indices, item)
 
-    Do this without using a "counting variable" --- in other words,
-    DON'T do this:
-
-        >>> count = 0
-        >>> for item in list:
-        ...     print(count)
-        ...     count = count + 1
-        ...
-
-    The output should look like this:
-
-        >>> print_indices(['apple', 'berry', 'cherry'])
-        apple 0
-        berry 1
-        cherry 2
-    """
-
-    print("Nothing at all")
-
+items = ['apple', 'berry', 'cherry']
+print_indices(items)
 
 def words_in_common(words1, words2):
-    """Return words that are shared between `words1` and `words2`.
+    """Return words that are shared between `words1` and `words2`. """
+    common_words = words1 & words2
+    if len(common_words) == 0: #doesn't seem like most concise way to do this, but it works
+        common_words = []
+    
+    print(common_words)
 
-    The returned words are sorted alphabetically.
+words1 = {'Python', 'Ape'}
+words2 = {'Lizard', 'Turtle'}
 
-    NOTE: For this problem, feel free to use other data structures we've
-    learned about in this class.
+words_in_common(words1, words2)
+    
+def every_other_item(mixed_list):
+    """Return every other item in `items`, starting at first item. """
+    every_other = mixed_list[0::2]
+    print(every_other)
 
-    For example:
+mixed_list = 'a', 400, True, 'b', 0
+every_other_item(mixed_list)
 
-        >>> words_in_common(
-        ...     ['Python', 'Python', 'Python'],
-        ...     ['Lizard', 'Turtle', 'Python']
-        ... )
-        ['Python']
+def smallest_n_items(nums, n):
+    """Return the `n` smallest integers in list in descending order."""
+    n = int(-(n + 1)) #this seems like an incorrect workaround, but it works the way I want it to
+    print(nums[n:-1])
 
-    The returned list should not have any duplicates:
+nums = [2, 6006, 700, 42, 6, 59, 3]
+nums.sort(reverse=True)
+print(nums)
+smallest_n_items(nums, 5)
 
-        >>> words_in_common(
-        ...     ['cheese', 'cheese', 'cheese', 'cake'],
-        ...     ['cheese', 'hummus', 'beets', 'cake']
-        ... )
-        ['cake', 'cheese']
-
-    If there are no words in common, return an empty list:
-
-        >>> words_in_common(
-        ...     ['lamb', 'chili', 'cheese'],
-        ...     ['cake', 'ice cream']
-        ... )
-        []
-    """
-
-    return ['the wrong thing']
-
-
-def every_other_item(items):
-    """Return every other item in `items`, starting at first item.
-
-    For example:
-
-       >>> every_other_item(['a', 400, True, 'b', 0])
-       ['a', True, 0]
-    """
-
-    return ['the wrong thing']
-
-
-def smallest_n_items(items, n):
-    """Return the `n` smallest integers in list in descending order.
-
-    You can assume that `n` will be less than the length of the list.
-
-    For example:
-
-        >>> smallest_n_items([2, 6006, 700, 42, 6, 59], 3)
-        [42, 6, 2]
-
-    If `n` is 0, return an empty list:
-
-        >>> smallest_n_items([3, 4, 5], 0)
-        []
-
-    Duplicates are OK:
-
-        >>> smallest_n_items([1, 1, 1, 1, 1, 1], 2)
-        [1, 1]
-    """
-
-    return []
